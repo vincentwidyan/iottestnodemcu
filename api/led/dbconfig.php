1805fe1fabbe392834be6b8d0399acf8de65d159
vincentwidyan <42277973+vincentwidyan@users.noreply.github.com>
2019-10-15 22:49:03 +0700

    Create dbconfig.php

---
+++ api/led/dbconfig.php
@@ -0,0 +1,13 @@
+<?php
+/****************************************************/
+/************** Created by : Vivek Gupta ************/
+/***************     www.vsgupta.in     *************/
+/***************     www.iotmonk.com     *************/
+/****************************************************/ 
+
+define('DB_USER', "id11230865_iottestnodemcu");     // Your database user name
+define('DB_PASSWORD', "sagan123");			// Your database password (mention your db password here)
+define('DB_DATABASE', "id11230865_iottestnodemcu"); // Your database name
+define('DB_SERVER', "localhost");			// db server (Mostly will be 'local' host)
+
+?>
